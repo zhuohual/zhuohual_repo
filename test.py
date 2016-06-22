138a11ce3c755c029f901221b351f26d7c028eb4
lizhuohua <zhuohual@cn.ibm.com>
2016-06-22 17:03:44 +0800

    test

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
-#!/usr/bin/env python
-#encoding: utf-8
-class myclass:
-  def __init__(self):
+#!/usr/bin/env python 
+#encoding: utf-8 
+import unittest 
+import myclass 
+class mytest(unittest.TestCase): 
+  ##初始化工作 
+  def setUp(self): 
+    self.tclass = myclass.myclass()
+    ##实例化了被测试模块中的类 
+  #退出清理工作 
+  def tearDown(self): 
     pass
-  def sum(self, x, y):
-    return x+y
-  def sub(self, x, y):
-    return x-y
+  #具体的测试用例，一定要以test开头 
+  def testsum(self): 
+    self.assertEqual(self.tclass.sum(1, 2), 3) 
+if __name__ =='__main__': 
+  unittest.main()
